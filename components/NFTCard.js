40cd33263b69b8e2019642d43fad729996d98948
Diego <dtellez95@gmail.com>
2022-07-31 21:24:08 +0100

    add subinfo components and fix constants issues

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,7 +1,9 @@
 import { View, Image } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
-
+import { Text } from 'react-native'
 import { COLORS, SIZES, SHADOWS, assets } from '../constants'
+import { CircleButton, RectButton } from './Button'
+import { SubInfo, EthPrice, NFTTitle } from './SubInfo'
 
 const NFTCard = ({ data }) => {
     const navigation = useNavigation();
@@ -13,7 +15,31 @@ const NFTCard = ({ data }) => {
             margin: SIZES.base,
             ...SHADOWS.dark
         }}>
-        </View>
+            <View style={{ width: "100%", height: 250 }}>
+                <Image
+                    source={data.image}
+                    resizeMode="cover"
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        borderTopLeftRadius: SIZES.font,
+                        borderTopRightRadius: SIZES.font,
+                    }}
+                />
+
+                <CircleButton imgUrl={assets.heart} right={10} top={10} />
+            </View>
+
+            <SubInfo />
+            <View style={{ width: "100%", padding: SIZES.font }}>
+                <NFTTitle
+                    title={data.name}
+                    subTitle={data.creator}
+                    titleSize={SIZES.large}
+                    subTitleSize={SIZES.small}
+                />
+            </View>
+        </View >
     )
 }
 
