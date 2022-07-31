779cc2595602d5aa83cdeb614e588a961b41a73a
Diego <dtellez95@gmail.com>
2022-07-31 20:15:04 +0100

    add constants folder

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, SafeAreaView, FlatList, Text } from 'react-native'
+
+import { COLORS, NFTData } from '../constants'
+
 const Home = () => {
     return (
         <Text>Home</Text>
