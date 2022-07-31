d0383014e37feeef3d182523b079e4b35ae88f31
Diego <dtellez95@gmail.com>
2022-07-31 20:43:40 +0100

    update dependencies

--- screens/Home.js
+++ screens/Home.js
@@ -7,8 +7,31 @@ import { NFTCard, HomeHeader, FocusedStatusBar } from '../components'
 const Home = () => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
-
-        </SafeAreaView>
+            <FocusedStatusBar backgroundColor={COLORS.primary} />
+            <View style={{ flex: 1 }}>
+                <View style={{ zIndex: 0 }}>
+                    <FlatList
+                        data={NFTData}
+                        renderItem={({ item }) => <NFTCard data={item} />}
+                        keyExtractor={(item) => item.id}
+                        showsVerticalScrollIndicator={false}
+                        ListHeaderComponent={<HomeHeader />}
+                    />
+                </View>
+                {/* This View will work as a background */}
+                <View style={{
+                    position: "absolute",
+                    top: 0,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    zIndex: -1,
+                }}>
+                    <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+                    <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+                </View>
+            </View>
+        </SafeAreaView >
     )
 }
 
