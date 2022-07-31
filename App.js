78275a45a092344e50bacbae81774a7b69c0b7d1
Diego <dtellez95@gmail.com>
2022-07-31 15:23:21 +0100

    fix require imports

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
@@ -18,8 +17,14 @@ const theme = {
 const App = () => {
 
   const [loaded] = useFonts({
-
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
   });
+
+  if (!loaded) return null;
 
   return (
     <NavigationContainer theme={theme}>
